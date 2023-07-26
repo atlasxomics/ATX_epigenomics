6ffd6db06e79077ad3f662dad55ba3ef473289c5
jpmcga <jpaulmcgann@gmail.com>
2023-07-26 11:38:08 -0400

    chore: cleanup split_r2, Snakefile

--- preprocessing/split_r2.py
+++ preprocessing/split_r2.py
@@ -1,41 +1,43 @@
-########################################################
-# Process R2 for cellranger-atac pipeline
-# revised Dec 2022 for spatial ATAC v2
-########################################################
+""" split R2 into new-R2, R3 for downstream processing
+"""
 
 import argparse
+import logging
+
 from Bio.SeqIO.QualityIO import FastqGeneralIterator
 from gzip import open as gzopen
+
+logging.basicConfig(
+    format="%(levelname)s - %(asctime)s - %(message)s", level=logging.INFO
+)
 
 seq_start = 117
 bc2_start, bc2_end = 22, 30
 bc1_start, bc1_end = 60, 68
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--input",
-	required=True,
-	help="input file")
-ap.add_argument("-o1", "--output_R3",
-	required=True,
-	help="output file R3")
-ap.add_argument("-o2",
-	"--output_R2",
-	required=True,
-	help="output file R2")
+ap.add_argument("-i", "--input", required=True, help="input file")
+ap.add_argument("-o1", "--output_R3", required=True, help="output file R3")
+ap.add_argument("-o2", "--output_R2", required=True, help="output file R2")
 args = vars(ap.parse_args())
+logging.info(f"Arguemnts: {args}")
 
-input_file_R2 = args["input"]
-output_file_R3 = args["output_R3"]
-output_file_R2 = args["output_R2"]
+input_path = args["input"]
+output_pathR3 = args["output_R3"]
+output_pathR2 = args["output_R2"]
 
-with gzopen(input_file_R2, "rt") as in_handle_R2, \
-	open(output_file_R3, "w") as out_handle_R3, \
-	open(output_file_R2, "w") as out_handle_R2:
-    for title, seq, qual in FastqGeneralIterator(in_handle_R2):
-        new_seq_R3 = seq[seq_start:]
-        new_qual_R3 = qual[seq_start:]
-        barcode = seq[bc2_start:bc2_end] + seq[bc1_start:bc1_end] # !!! BC2 + BC1
-        new_qual_R2 = qual[bc2_start:bc2_end] + qual[bc1_start:bc1_end]        
-        out_handle_R3.write("@%s\n%s\n+\n%s\n" % (title, new_seq_R3, new_qual_R3))
-        out_handle_R2.write("@%s\n%s\n+\n%s\n" % (title, barcode, new_qual_R2))
+logging.info("R2 parsing initiated...")
+with gzopen(input_path, "rt") as in_R2, \
+    open(output_pathR3, "w") as out_R3, \
+	open(output_pathR2, "w") as out_R2:
+    for title, seq, qual in FastqGeneralIterator(in_R2):
 
+        R3_seq = seq[seq_start:]
+        R3_qual = qual[seq_start:]
+        R2_seq = seq[bc2_start:bc2_end] + seq[bc1_start:bc1_end] 
+        R2_qual = qual[bc2_start:bc2_end] + qual[bc1_start:bc1_end]        
+
+        out_R3.write("@%s\n%s\n+\n%s\n" % (title, R3_seq, R3_qual))
+        out_R2.write("@%s\n%s\n+\n%s\n" % (title, R2_seq, R2_qual))
+
+logging.info("R2 parsing completed.")
